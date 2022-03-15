0cf37e9236da441d543510f7adb96b0358f389a8
Abdus Salam <81427711+suntechlab@users.noreply.github.com>
2022-03-15 19:02:38 +0600

    Create posts.js

---
+++ api/posts.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+var Blogs = require('../models/blogs');
+/**
+ * GET product list.
+ *
+ * @return product list | empty.
+ */
+router.get("/", async (req, res) => {
+  try {
+    res.json({
+      status: 200,
+      message: "Get data has successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Server error");
+  }
+});
+
+router.get('/allblogs', async (req, res) => {
+  var result = await Blogs.find({});
+  if (!result) {
+      return res.status(400).send('Blog not found');
+  }
+  res.json(result);
+});
+
+router.get('/:id', async (req, res) => {
+  var id = req.params.id
+  var result = await Blogs.findOne({slug: id});
+  if (!result) {
+      res.json({notFound:true});
+  }
+  res.json(result);
+});
+
+
+module.exports = router;
